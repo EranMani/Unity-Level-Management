04fd2673dab8d634f0681a3ed1f4203af7b75bba
Eran Mani <eranmanim@gmail.com>
2021-07-18 08:26:17 +0300

    Created menu script which will be attached to each manu in the project Created a menu manager that will add the menus at runtime

---
+++ Assets/Level_Management/Scripts/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LevelManagement
+{
+    // Each menu must contain a canvas object 
+    [RequireComponent(typeof(Canvas))]
+    public class Menu : MonoBehaviour
+    {
+        public void OnBackPressed()
+        {
+
+        }
+    }
+}
